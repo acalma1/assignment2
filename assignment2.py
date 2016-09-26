842c0da8c17bc68d4a76538ed079e16803bbb4d4
acalma1 <a.calma24@gmail.com>
2016-09-25 21:08:27 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -2,41 +2,43 @@ import sys
 import crypt
 
 
+
 def crackPass(usr, pw):
-	en_type = usr.split("$")[1]
-	segment = usr.split("$")[2]
-	salt = "$" + en_type + "$" + segment + "$"
+	#Parses through the password and determines the type of encryption hash
+	#the salt and password
+	en_type = usr.split("$")[1] 
+	salt = usr.split("$")[2] 
+	insalt = "$" + en_type + "$" + segment + "$"
 
-
+	#Iterates through a simple english dictionary then uses the crypt function 
+	#to encrypt every word, and if it finds a match then it displays the password
+	#and if none are found, then it displays "No password found"
 	with open("/usr/share/dict/american-english", "r") as eng_dict:
-		for line in eng_dict:
-			word = word.stip("\n")
+		for line in eng_dict.readlines():
+			word = word.strip("\n")
 			if (crypt.crypt(word, salt) ==  pw):
 				print "Password found for user:\t " + usr + " and password is:\t" + word +"\n"
 				sys.exit(0)
 			else: 
 				print "No password found"
-				exit 
-				
+				exit 			
+
 def main():
 
-	
 	try:
-		for line in open(sys.argv[2], "r") readlines():
-			line = line.replace("\n","").split(":")
-			if  not line[1] in ['*','!']:	
-				usr = line[0]
-				pw = line[1]
-				crackPass(usr, pw)
-					
+		with open(sys.argv[2], "r") as f: #This allows the user to enter the username and filepath of the shadow file
+			for line in f.readlines():	
+				line = line.replace("\n","").split(":")
+				if  not line[1] in ['*','!']:	
+					usr = line[0] #the username is stored here
+					pw = line[1]  #the user's unencrypted password 
+					crackPass(usr, pw)
+
+	#If the file isn't found, the user is thrown a file not found message and exits			
 	except IOError:
-		print ("File not found: "sys.argv[2])
+		print ("File not found: " + sys.argv[2])
 		exit(1)
-	
-			
-
-
-				 
 
 if __name__ == "__main__":
+
 	main()
